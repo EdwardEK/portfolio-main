8e4a88e3e0bf83d916319a276a396e6af3c16ece
Edward Kane <edwarde.kane@gmail.com>
2023-03-01 06:31:45 -0500

    update commit

--- index.js
+++ index.js
@@ -1,65 +1,10 @@
+// ---
 const hamMenuBtn = document.querySelector(".header__main-ham-menu-cont");
 const smallMenu = document.querySelector(".header__sm-menu");
 const headerHamMenuBtn = document.querySelector(".header__main-ham-menu");
-const headerHamMenuCloseBtn = document.querySelector(
-  ".header__main-ham-menu-close"
-);
+const headerHamMenuCloseBtn = document.querySelector(".header__main-ham-menu-close");
 const headerSmallMenuLinks = document.querySelectorAll(".header__sm-menu-link");
-const headerLogoConatiner = document.querySelector(".header__logo-container");
 
-var Text = function (el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = "";
-  this.tick();
-  this.isDeleting = false;
-};
-
-Text.prototype.tick = function () {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
-
-  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
-
-  var that = this;
-  var delta = 200 - Math.random() * 100;
-
-  if (this.isDeleting) {
-    delta /= 2;
-  }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === "") {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
-
-  setTimeout(function () {
-    that.tick();
-  }, delta);
-};
-
-window.onload = function () {
-  var elements = document.getElementsByClassName("typewrite");
-  for (var i = 0; i < elements.length; i++) {
-    var toRotate = elements[i].getAttribute("data-words");
-    var period = elements[i].getAttribute("data-period");
-    if (toRotate) {
-      new Text(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-};
 hamMenuBtn.addEventListener("click", () => {
   if (smallMenu.classList.contains("header__sm-menu--active")) {
     smallMenu.classList.remove("header__sm-menu--active");
@@ -84,7 +29,9 @@ for (let i = 0; i < headerSmallMenuLinks.length; i++) {
 }
 
 // ---
+const headerLogoContainer = document.querySelector(".header__logo-container");
 
-headerLogoConatiner.addEventListener("click", () => {
+headerLogoContainer.addEventListener("click", () => {
   location.href = "index.html";
 });
+
